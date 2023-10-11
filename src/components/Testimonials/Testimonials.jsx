a683cfa798c51ce57beaa63d5e84e34af7341134
Vivek Suryvanshi <vivek.suryavanshi@bhagirathtechnologies.com>
2023-10-11 17:24:10 +0530

    images and names in testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -30,7 +30,7 @@ const Testimonials = () => {
                 class="testimonial-img"
                 alt=""
               />
-              <h3>Saul Goodman</h3>
+              <h3>Vihaan Singh</h3>
               <h4>Ceo &amp; Founder</h4>
               <div class="stars">
                 <i class="bi bi-star-fill"></i>
@@ -58,8 +58,8 @@ const Testimonials = () => {
                 class="testimonial-img"
                 alt=""
               />
-              <h3>Saul Goodman</h3>
-              <h4>Ceo &amp; Founder</h4>
+              <h3>Rahul Dubey</h3>
+              <h4>Manager (TCS)</h4>
               <div class="stars">
                 <i class="bi bi-star-fill"></i>
                 <i class="bi bi-star-fill"></i>
@@ -86,8 +86,148 @@ const Testimonials = () => {
                 class="testimonial-img"
                 alt=""
               />
-              <h3>Saul Goodman</h3>
+              <h3>Gurneet Kaur</h3>
               <h4>Ceo &amp; Founder</h4>
+              <div class="stars">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+              </div>
+              <p>
+                <i class="bi bi-quote quote-icon-left"></i>
+                Proin iaculis purus consequat sem cure digni ssim donec
+                porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <i class="bi bi-quote quote-icon-right"></i>
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
+          {" "}
+          <div class="testimonials">
+            <div class="testimonial-item">
+              <img
+                src="./assets/images/testimonials/testi-4.jpg"
+                class="testimonial-img"
+                alt=""
+              />
+              <h3>Andrew Symonds</h3>
+              <h4>Delivery Manager</h4>
+              <div class="stars">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+              </div>
+              <p>
+                <i class="bi bi-quote quote-icon-left"></i>
+                Proin iaculis purus consequat sem cure digni ssim donec
+                porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <i class="bi bi-quote quote-icon-right"></i>
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+         <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
+          {" "}
+          <div class="testimonials">
+            <div class="testimonial-item">
+              <img
+                src="./assets/images/testimonials/testi-5.jpg"
+                class="testimonial-img"
+                alt=""
+              />
+              <h3>Mrs. Shruti Singh</h3>
+              <h4>Ceo &amp; Founder</h4>
+              <div class="stars">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+              </div>
+              <p>
+                <i class="bi bi-quote quote-icon-left"></i>
+                Proin iaculis purus consequat sem cure digni ssim donec
+                porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <i class="bi bi-quote quote-icon-right"></i>
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+         <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
+          {" "}
+          <div class="testimonials">
+            <div class="testimonial-item">
+              <img
+                src="./assets/images/testimonials/testi-6.jpg"
+                class="testimonial-img"
+                alt=""
+              />
+              <h3>Simran Kaur</h3>
+              <h4>HR & manager</h4>
+              <div class="stars">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+              </div>
+              <p>
+                <i class="bi bi-quote quote-icon-left"></i>
+                Proin iaculis purus consequat sem cure digni ssim donec
+                porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <i class="bi bi-quote quote-icon-right"></i>
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+         <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
+          {" "}
+          <div class="testimonials">
+            <div class="testimonial-item">
+              <img
+                src="./assets/images/testimonials/testi-7.jpg"
+                class="testimonial-img"
+                alt=""
+              />
+              <h3>Shubham Sharma</h3>
+              <h4>CTO & Manager</h4>
+              <div class="stars">
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+              </div>
+              <p>
+                <i class="bi bi-quote quote-icon-left"></i>
+                Proin iaculis purus consequat sem cure digni ssim donec
+                porttitora entum suscipit rhoncus. Accusantium quam, ultricies
+                eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
+                <i class="bi bi-quote quote-icon-right"></i>
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+         <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
+          {" "}
+          <div class="testimonials">
+            <div class="testimonial-item">
+              <img
+                src="./assets/images/testimonials/testi-8.jpg"
+                class="testimonial-img"
+                alt=""
+              />
+              <h3>Mr. Vivek Suryvanshi</h3>
+              <h4>Team Lead</h4>
               <div class="stars">
                 <i class="bi bi-star-fill"></i>
                 <i class="bi bi-star-fill"></i>
