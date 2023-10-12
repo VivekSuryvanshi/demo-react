7b4a4dbff6c8cf47168f342f1c05bf91f0aa4073
Vivek Suryvanshi <vivek.suryavanshi@bhagirathtechnologies.com>
2023-10-12 14:47:10 +0530

    replaced testimonial section with auto slider

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -10,45 +10,26 @@ import "swiper/css/effect-cards";
 import "./testimonials.css";
 
 // import required modules
-import { EffectCards } from "swiper";
+import { Autoplay,Pagination } from "swiper";
 // swiper/modules
 
 const Testimonials = () => {
   return (
     <>
       <Swiper
+      autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
         effect={"cards"}
         grabCursor={true}
-        modules={[EffectCards]}
+        modules={[Autoplay,Pagination]}
         className="testimonialSwiper"
       >
-        <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
-          <div class="testimonials">
-            <div class="testimonial-item">
-              <img
-                src="./assets/images/testimonials/testi-1.webp"
-                class="testimonial-img"
-                alt=""
-              />
-              <h3>Vihaan Singh</h3>
-              <h4>Ceo &amp; Founder</h4>
-              <div class="stars">
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-              </div>
-              <p>
-                <i class="bi bi-quote quote-icon-left"></i>
-                Proin iaculis purus consequat sem cure digni ssim donec
-                porttitora entum suscipit rhoncus. Accusantium quam, ultricies
-                eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                <i class="bi bi-quote quote-icon-right"></i>
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
+    
         <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
           {" "}
           <div class="testimonials">
@@ -200,34 +181,6 @@ const Testimonials = () => {
               />
               <h3>Shubham Sharma</h3>
               <h4>CTO & Manager</h4>
-              <div class="stars">
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-                <i class="bi bi-star-fill"></i>
-              </div>
-              <p>
-                <i class="bi bi-quote quote-icon-left"></i>
-                Proin iaculis purus consequat sem cure digni ssim donec
-                porttitora entum suscipit rhoncus. Accusantium quam, ultricies
-                eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.
-                <i class="bi bi-quote quote-icon-right"></i>
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-         <SwiperSlide className="testimonialSwiperSlide  SwiperSlide">
-          {" "}
-          <div class="testimonials">
-            <div class="testimonial-item">
-              <img
-                src="./assets/images/testimonials/testi-8.jpg"
-                class="testimonial-img"
-                alt=""
-              />
-              <h3>Mr. Vivek Suryvanshi</h3>
-              <h4>Team Lead</h4>
               <div class="stars">
                 <i class="bi bi-star-fill"></i>
                 <i class="bi bi-star-fill"></i>
