8e31a1076518f6bb8c5892a097a5103d1ad45ae9
Vivek Suryvanshi <vivek.suryavanshi@bhagirathtechnologies.com>
2023-10-30 17:17:34 +0530

    added agro products

--- src/pages/Products/AgroCommodities/AgroCommodities.jsx
+++ src/pages/Products/AgroCommodities/AgroCommodities.jsx
@@ -87,7 +87,7 @@ const AgroCommodities = () => {
         <div id="agro_commodities" className="container" data-aos="fade-up">
           <header className="section-header">
             <h3>Products We Serves</h3>
-            <p>
+            <p className="col-lg-10 col-12 mx-auto">
               Discover top-quality polymers at Bhagirath BVM Grand. As leading
               suppliers, we provide a wide range of innovative polymer
               solutions. Our commitment to excellence ensures industries receive
@@ -96,7 +96,7 @@ const AgroCommodities = () => {
               through advanced chemical solutions.
             </p>
           </header>
-          <LeftContent
+          {/* <LeftContent
             title={data[0].title}
             desc={data[0].desc}
             image={data[0].image}
@@ -131,7 +131,311 @@ const AgroCommodities = () => {
             desc={data[5].desc}
             image={data[5].image}
             properties={data[5].properties}
-          />
+          /> */}
+
+          {/* new code */}
+          <div class="timeline container col-xxl-10 col-xl-10 col-lg-10 col-md-12 col-12">
+            <div class="container product_card">
+              <div class="row">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 container_part left_container order-last order-lg-first">
+                  <div class="animated_icon_div">
+                    <img
+                      class="animated_icon img-fluid"
+                      src="../assets/images/icons/Agro Commodities/rice.webp"
+                      alt="product_images"
+                    />
+                  </div>
+                  <div class="text_box">
+                    <h2 class="text_card">RICE</h2>
+                    <p class="product_desc">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis expedita aliquam, excepturi reprehenderit dolorum
+                      suscipit. Ab quasi cumque corrupti libero.
+                    </p>
+                    <ul class="ul_list">
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Ullamco laboris nisi ut aliquip consequat
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Lorem ipsum dolor sit, amet consectetur adipisicin
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        In dignissimos libero aliquid non illo
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        sint accusamus placeat molestias corporis!
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        voluptatum nesciunt ex architecto neque exerct.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 d-flex align-items-center justify-content-end order-lg-last order-first">
+                  <img
+                    src="../assets/images/Products-all-img/Agro-Commodities/rice.webp"
+                    alt=""
+                    class="col-lg-10 col-11 img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="container product_card">
+              <div class="row">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 z-n1 product_container container_part d-flex align-items-center">
+                  <img
+                    src="../assets/images/Products-all-img/Agro-Commodities/soyabean.webp"
+                    class="col-lg-10 col-11 img-fluid"
+                    alt="product_images"
+                  />
+                  <div class="animated_icon_div">
+                    <img
+                      class="animated_icon"
+                      src="../assets/images/icons/Agro Commodities/soybean.webp"
+                      alt="product_images"
+                    />
+                  </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 right_container">
+                  <div class="text_box">
+                    <h2 class="text_card">SOYABEAN</h2>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis expedita aliquam, excepturi reprehenderit dolorum
+                      suscipit. Ab quasi cumque corrupti libero.
+                    </p>
+                    <ul class="ul_list">
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Ullamco laboris nisi ut aliquip consequat
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Lorem ipsum dolor sit, amet consectetur adipisicin
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        In dignissimos libero aliquid non illo
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        sint accusamus placeat molestias corporis!
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        voluptatum nesciunt ex architecto neque exerct.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="container product_card">
+              <div class="row">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 container_part left_container order-last order-lg-first">
+                  <div class="animated_icon_div">
+                    <img
+                      class="animated_icon"
+                      src="../assets/images/icons/Agro Commodities/sugar.webp"
+                      alt="product_images"
+                    />
+                  </div>
+                  <div class="text_box">
+                    <h2 class="text_card">SUGAR</h2>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis expedita aliquam, excepturi reprehenderit dolorum
+                      suscipit. Ab quasi cumque corrupti libero.
+                    </p>
+                    <ul class="ul_list">
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Ullamco laboris nisi ut aliquip consequat
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Lorem ipsum dolor sit, amet consectetur adipisicin
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        In dignissimos libero aliquid non illo
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        sint accusamus placeat molestias corporis!
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        voluptatum nesciunt ex architecto neque exerct.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 z-n1 d-flex align-items-center product_container justify-content-end order-lg-last order-first">
+                  <img
+                    src="../assets/images/Products-all-img/Agro-Commodities/sugar.webp"
+                    alt=""
+                    class="col-lg-10 col-11 img-fluid"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="container product_card">
+              <div class="row">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 z-n1 container_part d-flex align-items-center product_container">
+                  <img
+                    src="../assets/images/Products-all-img/Agro-Commodities/wheat.webp"
+                    alt=""
+                    class="col-lg-10 col-11 img-fluid"
+                  />
+                  <div class="animated_icon_div">
+                    <img
+                      class="animated_icon"
+                      src="../assets/images/icons/Agro Commodities/wheat.webp"
+                      alt="product_images"
+                    />
+                  </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 right_container">
+                  <div class="text_box">
+                    <h2 class="text_card">WHEAT</h2>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis expedita aliquam, excepturi reprehenderit dolorum
+                      suscipit. Ab quasi cumque corrupti libero.
+                    </p>
+                    <ul class="ul_list">
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Ullamco laboris nisi ut aliquip consequat
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Lorem ipsum dolor sit, amet consectetur adipisicin
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        In dignissimos libero aliquid non illo
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        sint accusamus placeat molestias corporis!
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        voluptatum nesciunt ex architecto neque exerct.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="container product_card">
+              <div class="row">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 container_part left_container order-last order-lg-first">
+                  <div class="animated_icon_div">
+                    <img
+                      class="animated_icon"
+                      src="../assets/images/icons/Agro Commodities/Dal & Pules.webp"
+                      alt="product_images"
+                    />
+                  </div>
+                  <div class="text_box">
+                    <h2 class="text_card">DAL AND PULES</h2>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis expedita aliquam, excepturi reprehenderit dolorum
+                      suscipit. Ab quasi cumque corrupti libero.
+                    </p>
+                    <ul class="ul_list">
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Ullamco laboris nisi ut aliquip consequat
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Lorem ipsum dolor sit, amet consectetur adipisicin
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        In dignissimos libero aliquid non illo
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        sint accusamus placeat molestias corporis!
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        voluptatum nesciunt ex architecto neque exerct.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 z-n1 d-flex align-items-center justify-content-end">
+                  <img
+                    src="../assets/images/Home-page-img/hero-section/sacks-healthy-legumes-grains.webp"
+                    class="col-lg-10 col-11 img-fluid"
+                    alt="product_images"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="container product_card">
+              <div class="row">
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 z-n1 container_part d-flex align-items-center product_container">
+                  <img
+                    src="../assets/images/Products-all-img/Agro-Commodities/yellow-maze.webp"
+                    class="col-lg-10 col-11 img-fluid"
+                    alt="product_images"
+                  />
+                  <div class="animated_icon_div">
+                    <img
+                      class="animated_icon"
+                      src="../assets/images/icons/Agro Commodities/Yellow maize.webp"
+                      alt="product_images"
+                    />
+                  </div>
+                </div>
+                <div class="col-xxl-6 col-xl-6 col-lg-6 col-12 right_container">
+                  <div class="text_box">
+                    <h2 class="text_card">YELLOW MAZE</h2>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Corporis expedita aliquam, excepturi reprehenderit dolorum
+                      suscipit. Ab quasi cumque corrupti libero.
+                    </p>
+                    <ul class="ul_list">
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Ullamco laboris nisi ut aliquip consequat
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        Lorem ipsum dolor sit, amet consectetur adipisicin
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        In dignissimos libero aliquid non illo
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        sint accusamus placeat molestias corporis!
+                      </li>
+                      <li>
+                        <i class="bi bi-check-lg"></i>
+                        voluptatum nesciunt ex architecto neque exerct.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
