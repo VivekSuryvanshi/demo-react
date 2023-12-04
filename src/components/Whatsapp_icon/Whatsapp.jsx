1d9f5fcc3dc00be833f2c13436481cbc0d7ff05a
Vivek Suryvanshi <vivek.suryavanshi@bhagirathtechnologies.com>
2023-12-04 18:33:33 +0530

    changed whatsapp no. in icon

--- src/components/Whatsapp_icon/Whatsapp.jsx
+++ src/components/Whatsapp_icon/Whatsapp.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Whatsapp = () => {
   return (
    <>
-    <Link to="https://api.whatsapp.com/send/?phone=9185239195807&amp;text=learn&amp;type=phone_number&amp;app_absent=0" className="whatsapp_float" target="_blank"><i className="bi bi-whatsapp my_float"></i></Link>
+    <Link to="https://api.whatsapp.com/send/?phone=85297468144&amp;text=learn&amp;type=phone_number&amp;app_absent=0" className="whatsapp_float" target="_blank"><i className="bi bi-whatsapp my_float"></i></Link>
    </>
   )
 }
